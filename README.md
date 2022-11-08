c74d41e1cfc0c985ed4d947fad4ed98df7ebb5d6
Jake <jakepucci26@gmail.com>
2022-11-08 14:32:28 -0700

    fix: header not centered

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Todo:
 
-- [ ] category Index
-- [ ] category Show
-- [ ] category Edit
-- [ ] location Index
-- [ ] location Show
-- [ ] location Edit
+- [x] category Index
+- [x] category Show
+- [x] category Edit
+- [x] location Index
+- [x] location Show
+- [x] location Edit
 - [ ] Redirect to Login
-- [ ] create sign up
-- [ ] set h-min 74 to fix little box in layout style 
+- [x] set h-min 74 to fix little box in layout style 
 - [ ] create report form 
 - [ ] need to add stats to top of dashboard
 
--- client/components/layout.js
+++ client/components/layout.js
@@ -47,7 +47,7 @@ export const Layout = ({ children, title }) => {
           <Disclosure as="nav" className="bg-gray-800">
             {({ open }) => (
               <>
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="max-w-7xl sm:px-6 lg:px-8">
                   <div className="border-b border-gray-700">
                     <div className="flex h-16 items-center justify-between px-4 sm:px-0">
                       <div className="flex items-center">
@@ -175,7 +175,7 @@ export const Layout = ({ children, title }) => {
             )}
           </Disclosure>
           <header className="py-10">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl px-4 sm:px-6 lg:px-8">
               <h1 className="text-3xl font-bold tracking-tight text-white">
                 {title}
               </h1>
